9cc2e9b135c44a13e29b72e2f9b47d51546b32ef
Priscila Nunes <103075521+ppnunes@users.noreply.github.com>
2023-08-01 21:49:28 -0300

    primeira aula de bdII

---
+++ Banco de Dados/sql_rh.sql
@@ -0,0 +1,20 @@
+create database bd_rh;
+use bd_rh;
+create table tb_cargo
+	(cd_cargo int not null primary key,
+    cargo char(30));
+create table tb_setor
+	(cd_setor int not null primary key,
+    setor char(30));
+create table tb_funcionario
+	(matricula int not null primary key,
+	funcionario char(50),
+	cd_setor int,
+	cd_cargo int,
+	salario decimal(12,2));
+alter table tb_funcionario
+	add constraint fk_setor foreign key (cd_setor)
+		references tb_setor (cd_setor);
+alter table tb_funcionario
+	add constraint fk_cargo  foreign key (cd_cargo)
+		references tb_cargo (cd_cargo);
